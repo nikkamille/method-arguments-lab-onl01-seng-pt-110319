e796a76879d8617bc510001c40592886d81a4634
Kamille Longalong <k.pagibigan@gmail.com>
2019-11-07 09:57:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,5 +9,5 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 
-introduction_with_language("Dan", "Ember.js")
+introduction_with_language("Dan", "Ember.js\n")
 
